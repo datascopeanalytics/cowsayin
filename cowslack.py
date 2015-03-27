3e0f1fcf1bb9f9853cf82d265779dfa33c1c1f60
stringertheory <mike.stringer@datascopeanalytics.com>
2015-03-26 23:13:25 -0500

    Got it working

--- cowslack.py
+++ cowslack.py
@@ -1,9 +1,19 @@
+import sys
+
 from flask import Flask
+from flask import request
+
+import cowsay
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", methods=['GET', 'POST'])
+def moo():
+    if request.method == 'GET':
+        return "Moo."
+    else:
+        text = request.form.get('text', 'Moo.')
+        return "```%s```\n" % cowsay.cowsay(text)
 
 if __name__ == "__main__":
     app.run()
--- cowslack.wsgi
+++ cowslack.wsgi
@@ -1,3 +1,3 @@
 import sys
-sys.path.insert(0, '/srv/www/just.cowsay.in'
+sys.path.insert(0, '/srv/www/just.cowsay.in')
 from cowslack import app as application
