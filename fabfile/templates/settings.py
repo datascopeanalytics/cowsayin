6ba4856f924d7076cc29828336beb13612a5bd83
stringertheory <mike.stringer@datascopeanalytics.com>
2015-03-30 11:14:34 -0500

    Updated settings file template

--- fabfile/templates/settings.py
+++ fabfile/templates/settings.py
@@ -1,3 +1,11 @@
-WEBHOOK_URL = '{{webhook_url}}'
+SECRET_KEY = {{secret_key}}
+
+WEBHOOK_URL = {{webhook_url}}
 
 DEBUG = {{debug}}
+
+GOOGLE_ANALYTICS = {{google_analytics}}
+
+DOMAIN = {{domain}}
+
+TRACKING = GOOGLE_ANALYTICS and not DEBUG
